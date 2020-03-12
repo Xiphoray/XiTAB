4388dc607f48eb4af2f8c1299b007378e5145a4c
Xiphoray <a351014842@gmail.com>
2020-03-12 22:55:47 +0800

    finish the copyright of pic and the weather

--- scripts/xitab.js
+++ scripts/xitab.js
@@ -3,7 +3,6 @@ function getBase64(url, callback){
     var dataURL = '';
     Img.setAttribute('crossorigin', 'anonymous');
     Img.src = url;
-
     Img.onload = () => {
         var canvas = document.createElement("canvas");
         var width = Img.width;
@@ -16,44 +15,94 @@ function getBase64(url, callback){
     }
 }
 
+function getWeather(){
+    if(Date.parse(new Date()) - localStorage.getItem("saveWtime") > 10*60*1000){
+        var loca = localStorage.getItem("location");
+        var weatherurl = "";
+        if(loca == "auto" || loca == null){
+            weatherurl = 'https://free-api.heweather.net/s6/weather/now?location=auto_ip&key=606383f7a03b43939e5bff187c8999ca';
 
-$(document).ready(function() {
-
-    if(localStorage.getItem("backimg") == null){
-        document.getElementById("xibackground").style.backgroundImage = "url('image/mountain.jpg')";
+        }else{
+            weatherurl = 'https://free-api.heweather.net/s6/weather/now?location=' + loca + '&key=606383f7a03b43939e5bff187c8999ca';
+        }
+        try{
+            $.ajax({url:weatherurl,
+                    dataType:"json",
+                    type:"GET",
+                    success:function(weatherss){
+                        if(weatherss.HeWeather6[0].status != "ok"){
+                            localStorage.setItem("location","auto");
+                            return;
+                        }
+                        localStorage.setItem("weatvalue",weatherss.HeWeather6[0].now.cond_txt);
+                        localStorage.setItem("tempvalue",weatherss.HeWeather6[0].now.tmp);
+                        localStorage.setItem("saveWtime",Date.parse(new Date()));
+                        
+                    }
+                }) 
+            }catch(err){
+                console.log(err);
+                localStorage.setItem("location","auto");
+            }
     }
-    else{
+}
 
-        document.getElementById("xibackground").style.backgroundImage = "url('" + localStorage.getItem("backimg") + "')";
-    }
-
-    if(navigator.onLine){
-        chrome.downloads.setShelfEnabled(true);
-        jQuery.support.cors = true;
+function getPic(){
+    if(Date.parse(new Date()) - localStorage.getItem("savePtime") > 60*60*1000){
         $.ajax({url:'https://bing.ioliu.cn/v1/rand?w=1920&h=1080',
             dataType:"jsonp",
             jsonp:"callback",
             jsonpCallback:"jsonCallback",
             success:function(imagess){
                 var reg = /(.*).jpg/;
-                console.log(imagess);
-                var jj = imagess;
-                var obj = jj.data;
-                var storurl = "";
-                getBase64(reg.exec(obj.url)[1].trim()+".jpg", (storurl) => {
+                getBase64(reg.exec(imagess.data.url)[1].trim()+".jpg", (storurl) => {
                     localStorage.setItem("backimg",storurl);
+                    localStorage.setItem("imgcr",imagess.data.copyright);
+                    localStorage.setItem("savePtime",Date.parse(new Date()));
                 })
-
-
             }
-
-        })
-        
-
-        
+        }) 
     }
+}
+
+
+$(document).ready(function() {
     
-
+    if(localStorage.getItem("backimg") == null){
+        document.getElementById("xibackground").style.backgroundImage = "url('image/mountain.jpg')";
+        localStorage.setItem("savePtime",0);
+        localStorage.setItem("saveWtime",0);
+        localStorage.setItem("location","auto");
+    }
+    else{
+        document.getElementById("xibackground").style.backgroundImage = "url('" + localStorage.getItem("backimg") + "')";
+        document.getElementById("photoli").innerHTML = localStorage.getItem("imgcr");
+        document.getElementById("weatvalue").innerHTML = localStorage.getItem("weatvalue");
+        document.getElementById("tempvalue").innerHTML = localStorage.getItem("tempvalue") + '&#176';
+    }
+    if(navigator.onLine){
+        jQuery.support.cors = true;
+        getWeather();
+        getPic();
+        if(localStorage.getItem("backimg").length < 1.5*1024*1024){
+            localStorage.setItem("savePtime",0);
+            getPic();
+        }
+    }
+    document.getElementById("mask").onclick = function(){
+        document.getElementById("locacvalue").value = localStorage.getItem("location");
+        document.getElementById("locac").style.display = "block";
+    }
+    document.getElementById("maskbuttom").onclick = function(){
+        var locavalue = document.getElementById("locacvalue").value;
+        localStorage.setItem("location",locavalue);
+        localStorage.setItem("saveWtime",0);
+        getWeather();
+        document.getElementById("locac").style.display = "none";
+        setTimeout(function(){
+            document.getElementById("weatvalue").innerHTML = localStorage.getItem("weatvalue");
+            document.getElementById("tempvalue").innerHTML = localStorage.getItem("tempvalue") + '&#176';
+        }, 4000);
+    }
 });
-
 
